d313f33d77fc67ab2837550064b9ce6c008b7c99
Ingrid M Miranda <ingridmoreiramiranda@gmail.com>
2022-02-12 18:54:07 -0300

    Ajustes finais

--- multiplesCsvIntoOne.py
+++ multiplesCsvIntoOne.py
@@ -1,18 +1,19 @@
 import os
 import glob
 import pandas as pd
-#set working directory
+
+#seta o local
 path = "/home/ingridmoreira/Documentos/Arquivos_Finais"
 
-#find all csv files in the folder
-#use glob pattern matching -> extension = 'csv'
-#save result in list -> all_filenames
-#extension = 'csv'
+#listar .csv de diretório
+#percorre diretório -> 'glob'
+#salva na lista -> all_filenames
 all_filenames = [i for i in glob.glob(path + "/*.csv")]
 #print(all_filenames)
 
-#combine all files in the list
+#combina todos os .csv do diretório em um só
 df = [pd.read_csv(f, header=None, sep='delimiter', engine='python') for f in all_filenames]
 combined_csv = pd.concat(df)
-#export to csv
+
+#na exportação nome pode ser modificado
 combined_csv.to_csv( "csv_final.csv", index=False, encoding='utf-8-sig')
